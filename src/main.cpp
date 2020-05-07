79048c3b4846f7f1e3d1ca0dbbfa785278291f6a
Adeel Zahid <47818499+muhammadadeelzahid@users.noreply.github.com>
2020-05-07 21:00:31 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@
 void MoveBulletsTimed(sf::Clock &clock, Interface &game);
 void timedIncrement(sf::Clock &clock2, Interface &game);
 void ChangeStateDelay(sf::Clock &clock3, Interface &game);
+void EventHandle(sf::Event &event,sf::RenderWindow &window, Interface &game);
+
 int main() {
 	sf::Clock clock;
 	sf::Clock clock2;
@@ -27,81 +29,88 @@ int main() {
 
 	sf::RenderWindow stat(sf::VideoMode(400, 250), "Game Stats");
 	stat.setPosition(sf::Vector2i(10, 200));
-	game.drawMaze();
 	sf::RenderWindow window(sf::VideoMode(sizeForBar, sizeScreen), "Game");
 	window.setPosition(sf::Vector2i(window.getPosition().x + 100, window.getPosition().y));
+
+
+	game.drawMaze();
 	while (window.isOpen()) {
 		sf::Color background(220, 220, 220);
 		window.clear(background);
 
 		sf::Event event;
 		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window.close();
-			if (event.type == sf::Event::KeyPressed) {
-				if (game.getPause() == 0 && game.getChangeStateDelay() == 0) {
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::L)) {
-						game.fire(0);
-						cout << "Fire tank 1" << endl;
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::C)) {
-						game.fire(1);
-						cout << "Fire Tank 2" << endl;
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-						game.moveTank("Up", 1); // Move tank Number 1 upwards //index of arrays start from 0
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-						game.moveTank("Down", 1); // Move tank Number 1 Down
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-						game.moveTank("Right", 1); // Move tank Number 1 Right
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-						game.moveTank("Left", 1); // Move tank Number 1 Left
-					}
-					//logic for movement starts
-
-					//make changes here below to control the movement
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-						game.moveTank("Up", 0); // Move tank Number 1 upwards //index of arrays start from 0
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-						game.moveTank("Down", 0); // Move tank Number 1 Down
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-						game.moveTank("Right", 0); // Move tank Number 1 Right
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-						game.moveTank("Left", 0); // Move tank Number 1 Left
-					}
-				}
-				//for pause
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::P)) {
-					game.setPause(1 - game.getPause());
-					//cout<<"Pause status: "<<game.getPause()<<endl;
-				}
-
-			}
-
+			EventHandle(event,window,game);
 		}
-		if (game.getPause() == 1)
-			game.Maze_Change_And_Pause_Message(window);
+
 		if (game.getPause() == 0 || game.getChangeStateDelay() != 0) {
 			game.setMineCoordinates();
 			MoveBulletsTimed(clock, game);
 			game.BulletscollisionWithTank();
 		}
 		game.display(window);
-		if (game.getChangeStateDelay() != 0) {
+		if (game.getChangeStateDelay() != 0 || game.getPause() == 1) {
 			game.Maze_Change_And_Pause_Message(window);
 		}
 
 		ChangeStateDelay(clock3, game);
 		window.display();
 		timedIncrement(clock2, game);
+
 	}
+
 	return 0;
+}
+void EventHandle(sf::Event &event,sf::RenderWindow &window, Interface &game)
+{
+
+	if (event.type == sf::Event::Closed)
+		window.close();
+	if (event.type == sf::Event::KeyPressed) {
+		if (game.getPause() == 0 && game.getChangeStateDelay() == 0) {
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::L)) {
+				game.fire(0);
+				cout << "Fire tank 1" << endl;
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::C)) {
+				game.fire(1);
+				cout << "Fire Tank 2" << endl;
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+				game.moveTank("Up", 1); // Move tank Number 1 upwards //index of arrays start from 0
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+				game.moveTank("Down", 1); // Move tank Number 1 Down
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+				game.moveTank("Right", 1); // Move tank Number 1 Right
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+				game.moveTank("Left", 1); // Move tank Number 1 Left
+			}
+			//logic for movement starts
+
+			//make changes here below to control the movement
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+				game.moveTank("Up", 0); // Move tank Number 1 upwards //index of arrays start from 0
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+				game.moveTank("Down", 0); // Move tank Number 1 Down
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+				game.moveTank("Right", 0); // Move tank Number 1 Right
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+				game.moveTank("Left", 0); // Move tank Number 1 Left
+			}
+		}
+		//for pause
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::P)) {
+			game.setPause(1 - game.getPause());
+			//cout<<"Pause status: "<<game.getPause()<<endl;
+		}
+
+	}
 }
 void timedIncrement(sf::Clock &clock2, Interface &game) {
 	if (clock2.getElapsedTime() > sf::milliseconds(100)) {
